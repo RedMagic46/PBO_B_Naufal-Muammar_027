6b77aac4550e2c50e9d4166b669261b5cebb041e
RedMagic46 <naufalmuamar46@gmail.com>
2025-03-08 18:35:51 +0700

    Praktikum

---
+++ Praktikum PBO/Codelab/Modul 2/Codelab1/src/Hewan.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Hewan {
+    String nama, jenis, suara;
+
+    void tampilkanInfo(){
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis: " + jenis);
+        System.out.println("Suara: " + suara + "\n");
+    }
+}
